25d7f76e8607d87d3e75167ea6d99b6da2b5791a
ruben-gutierrez-nombela <ruben.gutierreznombela@tiernoparla.es>
2023-02-02 17:54:41 +0100

    Estructura menu

---
+++ practica2/src/test/java/com/practica2/AppTest.java
@@ -0,0 +1,23 @@
+package com.practica2;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    public AppTest() {
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
