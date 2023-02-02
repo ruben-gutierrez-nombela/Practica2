45d4c9071b479aad09ce054df2182371a776b3b1
ruben-gutierrez-nombela <ruben.gutierreznombela@tiernoparla.es>
2023-02-02 18:14:50 +0100

    Printeo y estructura del menu

--- practica2/src/main/java/com/practica2/Menu.java
+++ practica2/src/main/java/com/practica2/Menu.java
@@ -15,19 +15,21 @@ public class Menu {
     private static final String OPCION_5_MENU = "5. Obtener precio de venta de una obra";
     private static final String OPCION_6_MENU = "6. Imprimir etiqueta de una obra";
     private static final String OPCION_SALIR_MENU = "0. SALIR";
-    private static final String OPCIONES_MENU = {OPCION_1_MENU, OPCION_2_MENU, OPCION_3_MENU, OPCION_4_MENU
-                                                OPCION_5_MENU, OPCION_6_MENU, SALIR};
+    private static final String[] OPCIONES_MENU = {OPCION_1_MENU, OPCION_2_MENU, OPCION_3_MENU, OPCION_4_MENU, OPCION_5_MENU, OPCION_6_MENU, OPCION_SALIR_MENU};
 
     private static final int DECIR_1 = 1;
     private static final int DECIR_2 = 2;
     private static final int DECIR_3 = 3;
+    private static final int DECIR_4 = 4;
+    private static final int DECIR_5 = 5;
+    private static final int DECIR_6 = 6;
     private static final int SALIR = 0;
     private static final String MENSAJE_ERROR_USER = "Opción elegida incorrecta";
     private static final String MENSAJE_DESPEDIDA = "¡Gracias por usar la aplicación! Hasta pronto.";
 
 
     public static void main(String[] args){
-
+    
         Scanner sc = new Scanner(System.in);
 
         while (!finalizar) {
